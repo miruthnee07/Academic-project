6063749739464995689c214378988201d94c337d
miruthnee07 <miru200709@gmail.com>
2026-07-28 21:14:23 +0530

    Create User.java

---
+++ User.java
@@ -0,0 +1,29 @@
+package com.microlending.model;
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private String fullName;
+
+    public User() {}
+
+    public User(int id, String username, String password, String fullName) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.fullName = fullName;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getFullName() { return fullName; }
+    public void setFullName(String fullName) { this.fullName = fullName; }
+}
